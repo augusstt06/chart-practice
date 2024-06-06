0b4b60b87e3536d81d3cf5bee4172cdd2b64c66d
augusstt06 <augusstt06@gmail.com>
2024-06-06 19:01:12 +0900

    chart component

---
+++ src/app/_components/chart/Chart.tsx
@@ -0,0 +1,54 @@
+// import {
+//   BarSeries,
+//   CandlestickSeries,
+//   Chart,
+//   ChartCanvas,
+//   CrossHairCursor,
+//   CurrentCoordinate,
+//   discontinuousTimeScaleProviderBuilder,
+//   EdgeIndicator,
+//   elderRay,
+//   ElderRaySeries,
+//   ema,
+//   lastVisibleItemBasedZoomAnchor,
+//   LineSeries,
+//   MouseCoordinateX,
+//   MouseCoordinateY,
+//   MovingAverageTooltip,
+//   OHLCTooltip,
+//   SingleValueTooltip,
+//   XAxis,
+//   YAxis,
+//   ZoomButtons,
+// } from 'react-financial-charts'
+
+// import { format } from 'd3-format'
+// import { timeFormat } from 'd3-time-format'
+
+const ChartComponent = () => {
+  //   const ScaleProvider =
+  //     discontinuousTimeScaleProviderBuilder().inputDateAccessor(
+  //       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  //       (d) => new Date(d.date),
+  //     )
+  //   const height = 700
+  //   const width = 900
+  //   const margin = { left: 0, right: 48, top: 0, bottom: 24 }
+  return (
+    <></>
+    // <ChartCanvas
+    //   height={height}
+    //   ratio={3}
+    //   width={width}
+    //   margin={margin}
+    //   data={data}
+    //   displayXAccessor={undefined}
+    //   seriesName="Data"
+    //   xScale={xScale}
+    //   xAccessor={xAccessor}
+    //   xExtents={xExtents}
+    //   zoomAnchor={lastVisibleItemBasedZoomAnchor}
+    // />
+  )
+}
+export default ChartComponent
